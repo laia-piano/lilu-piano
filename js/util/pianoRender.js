07fc45835013f784f3e41e7f904b08dac8f2797a
ephraimguo <ephraimguo@gmail.com>
2021-10-15 15:24:18 +0800

    commit for get filter done

--- js/util/pianoRender.js
+++ js/util/pianoRender.js
@@ -1,13 +1,20 @@
 // 1. render list and append
 
+let cachedList = [];
+
 function renderList (json, domId, domcument) {
     const node = domcument.querySelector("#piano-list-wrapper");
     node.innerHTML = "";
     console.log(json);
+    cachedList = json;
+
+    if (!json) {
+        return;
+    }
 
     let component = '';
     json.forEach(piano => {
-        const {uri, brand, price} = piano;
+        const {uri, modelName, price, brand} = piano;
         component += `
                     <li class="product type-product has-post-thumbnail column-1_3">
                       <div class="post_item_wrap">
@@ -18,7 +25,7 @@ function renderList (json, domId, domcument) {
                           </div>
                         </div>
                         <div class="post_content">
-                          <h3><a href="product.html">${brand}</a></h3>
+                          <h3><a href="product.html">${modelName} - ${brand}</a></h3>
                           <span class="price"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">&#36;</span>${price}</span>
                           </span>
                           <a rel="nofollow" href="product.html" class="button product_type_simple add_to_cart_button ajax_add_to_cart">View Detail</a> </div>
@@ -38,11 +45,136 @@ function renderList (json, domId, domcument) {
 //      - sort by price
 //      - sort by date
 //      -
-// const sortList = () => {
-//
-//
-// }
-// // module.exports = {
-//     renderList, sortList
-// };
+function sortList(document, sortBy, id) {
+    let elem = document.getElementById(id);
+    // console.log(elem);
+    elem.addEventListener('change', function (evt) {
+        let direction = evt.target.value;
 
+        let sortedJson = _decider(cachedList, sortBy, direction);
+        return renderList(sortedJson, '#piano-list-wrapper', document);
+    });
+
+}
+
+function filterList(json, document, filterBy, id) {
+    console.log(arguments);
+
+    let elem = document.getElementById(id);
+    let filteredList = [];
+
+    if (filterBy == 'price') {
+        elem.onmousemove = function (evt) {
+            console.log(evt);
+
+            let r = /\d+/;
+
+            let from = document.querySelector('span.from').innerHTML.match(r)[0];
+            let to = document.querySelector('span.to').innerHTML.match(r)[0];
+
+            console.log(from);
+            console.log(to);
+
+            filteredList = json.filter(piano => piano[filterBy] >= from && piano[filterBy] <= to);
+            cachedList = filteredList;
+            return renderList(filteredList, '#piano-list-wrapper', document);
+
+        };
+    }
+
+    if (filterBy == 'brand') {
+        let elems = document.getElementsByClassName('brand-anchor');
+
+        console.log(elems);
+
+        if (!elems) {
+            return;
+        }
+
+        [...elems].forEach(elem => {
+            console.log(elem);
+            elem.onclick = function (evt) {
+                evt.preventDefault();
+                // console.log(filterBy);
+                // console.log(elem.innerHTML);
+                filteredList = json.filter(piano => {
+                    console.log('\t', piano[filterBy], elem.innerText);
+                    return piano[filterBy].toLowerCase() == elem.innerText.toLowerCase();
+                });
+
+                // for (j of json) {
+                //     if (j[filterBy].toLowerCase() == elem.innerText.toLowerCase()) {
+                //         filteredList = [...filteredList, j];
+                //     }
+                // }
+
+                console.log(filteredList);
+                cachedList = filteredList;
+                return renderList(filteredList, '#piano-list-wrapper', document);
+
+            }
+        });
+
+        // for (elem of elems) {
+        //     console.log(elem);
+        //     elem.onclick = function (evt) {
+        //         evt.preventDefault();
+        //         // console.log(filterBy);
+        //         // console.log(elem.innerHTML);
+        //         filteredList = json.filter(piano => {
+        //             console.log('\t', piano[filterBy], elem.innerText);
+        //             return piano[filterBy].toLowerCase() == elem.innerText.toLowerCase();
+        //         });
+        //
+        //         // for (j of json) {
+        //         //     if (j[filterBy].toLowerCase() == elem.innerText.toLowerCase()) {
+        //         //         filteredList = [...filteredList, j];
+        //         //     }
+        //         // }
+        //
+        //         console.log(filteredList);
+        //         cachedList = filteredList;
+        //         return renderList(filteredList, '#piano-list-wrapper', document);
+        //
+        //     }
+        // }
+    }
+
+
+}
+
+function _decider(json, sortBy, direction) {
+    let sorted;
+    if (!json) {
+        return;
+    }
+
+    if (direction == 'ascending') {
+        sorted = json.sort(function (a, b) {
+            if (a[sortBy] > b[sortBy]) {
+                return 1;
+            } else if (a[sortBy] < b[sortBy]) {
+                return -1;
+            } else {
+                return 0;
+            }
+        });
+    }
+
+    if (direction == 'descending') {
+        sorted = json.sort(function (a, b) {
+            if (a[sortBy] < b[sortBy]) {
+                return 1;
+            } else if (a[sortBy] > b[sortBy]) {
+                return -1;
+            } else {
+                return 0;
+            }
+        });
+    }
+    return sorted;
+}
+
+function test(evt) {
+    console.log(evt);
+}
--- static/piano.js
+++ static/piano.js
@@ -1,14 +1,44 @@
 const pianosList = [
-  {
-    "uri": "../images/product-1-300x300.jpg",
-    "price": 349.99,
-    "date": "2021-10-10",
-    "brand": "Dreadnought Acoustic Guitar"
-  },
-  {
-    "uri": "../images/product-1-300x300.jpg",
-    "price": 349.99,
-    "date": "2021-10-10",
-    "brand": "Dreadnought Acoustic Guitar"
-  }
+    {
+        "uri": "../images/product-1-300x300.jpg",
+        "price": 349.99,
+        "date": "2021-10-10",
+        "modelName": "M170",
+        "brand": "Dreadnought"
+    },
+    {
+        "uri": "../images/product-1-300x300.jpg",
+        "price": 1000.99,
+        "date": "2021-10-10",
+        "modelName": "S123",
+        "brand": "Steinway & Sons"
+    },
+    {
+        "uri": "../images/product-1-300x300.jpg",
+        "price": 1001.99,
+        "date": "2021-10-10",
+        "modelName": "M10",
+        "brand": "Blüthner"
+    },
+    {
+        "uri": "../images/product-1-300x300.jpg",
+        "price": 10000.99,
+        "date": "2021-10-10",
+        "modelName": "HQ300",
+        "brand": "YAMAHA"
+    },
+    {
+        "uri": "../images/product-1-300x300.jpg",
+        "price": 10020.99,
+        "date": "2021-10-10",
+        "modelName": "HQ100",
+        "brand": "YAMAHA"
+    },
+    {
+        "uri": "../images/product-1-300x300.jpg",
+        "price": 1004.99,
+        "date": "2021-10-10",
+        "modelName": "N132",
+        "brand": "OHHASHI"
+    }
 ];
