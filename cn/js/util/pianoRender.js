d6a10b29fa6e6c1c40c8be6ef16d9f966c00405a
lilu <laiapianosg@gmail.com>
2021-11-25 14:12:58 +0800

    new change

---
+++ cn/js/util/pianoRender.js
@@ -0,0 +1,179 @@
+document.write('<script src="static/pianoCN.js" type="text/javascript"></script>');
+
+// 1. render list and append
+// 1.1 test for commit
+let cachedList = [];
+let currentBrand = '';
+let currentPriceMax = 200000;
+let currentPriceMin = 0;
+let sortDir = 'descending';
+
+function renderList (json, domId, domcument) {
+    let renderingList = json;
+
+    const node = domcument.querySelector("#piano-list-wrapper");
+    node.innerHTML = "";
+    console.log('render list', renderingList);
+
+    if (!renderingList) {
+        return;
+    }
+
+    renderingList = _sortDecider(renderingList, 'price', sortDir);
+    cachedList = renderingList;
+
+    let component = '';
+    renderingList.forEach(piano => {
+        const {uri, modelName, price, brand,link} = piano;
+        component += `
+                    <li class="product type-product has-post-thumbnail column-1_3">
+                      <div class="post_item_wrap">
+                        <div class="post_featured">
+                          <div class="post_thumb">
+                            <a class="hover_icon hover_icon_link" href="${link}" target="_blank">
+                              <img width="300" height="300" src="${uri}" alt="product-7" title="product-7" /> </a>
+                          </div>
+                        </div>
+                        <div class="post_content">
+                          <h3>${brand} <br> ${modelName}</h3>
+                          <span class="price"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">&#36;</span>${price}</span>
+                          </span>
+                          <a rel="nofollow" href="${link}" target="_blank" class="button product_type_simple add_to_cart_button ajax_add_to_cart">查看详细</a> </div>
+                      </div>
+                    </li>
+`;
+    });
+
+    node.insertAdjacentHTML('afterbegin', component);
+
+    return component;
+}
+
+// 2. sorting function
+//      - sort by price
+//      - sort by date
+//      -
+function addSortListener(document, sortBy, id) {
+    let elem = document.getElementById(id);
+    elem.addEventListener('change', function (evt) {
+        let direction = evt.target.value;
+
+        sortDir = direction;
+
+        let sortedJson = _sortDecider(cachedList.length === 0 ? pianosList : cachedList, sortBy, direction);
+        cachedList = sortedJson;
+
+        return renderList(sortedJson, '#piano-list-wrapper', document);
+    });
+}
+
+function addFilterListener(json, document) {
+
+    const filterButton = document.querySelector('#price-filter-confirm-button');
+
+    filterButton.onclick = function (evt) {
+        let filteredList = [];
+
+        console.log(evt);
+
+        let r = /\d+/;
+
+        let from = document.querySelector('span.from').innerHTML.match(r)[0];
+        let to = document.querySelector('span.to').innerHTML.match(r)[0];
+
+        currentPriceMax = Number.parseInt(to);
+        currentPriceMin = Number.parseInt(from);
+
+        console.log(currentPriceMin);
+        console.log(currentPriceMax);
+        cachedList = filteredList;
+
+        console.log('evt: ', evt, currentBrand);
+        if (currentBrand !== '') {
+            filteredList = _filterWithPrice(_filterWithBrand(pianosList));
+        } else {
+            filteredList = _filterWithPrice(pianosList);
+        }
+        return renderList(filteredList, '#piano-list-wrapper', document);
+    };
+
+    let brandLinks = document.getElementsByClassName('brand-anchor');
+
+    if (!brandLinks) {
+        return;
+    }
+
+    [...brandLinks].forEach(brandLink => {
+        brandLink.onclick = function (evt) {
+            let filteredList = [];
+
+            console.log(brandLink);
+            console.log('currentbrand-old:' + currentBrand);
+            evt.preventDefault();
+
+            currentBrand = brandLink.innerText.toLowerCase();
+
+            cachedList = filteredList;
+            console.log('currentbrand-new:' + currentBrand);
+            if (currentBrand !== '') {
+                filteredList = _filterWithPrice(_filterWithBrand(pianosList));
+            } else {
+                filteredList = _filterWithPrice(pianosList);
+            }
+
+            console.log('brand filter' , filteredList);
+            return renderList(filteredList, '#piano-list-wrapper', document);
+        }
+    });
+}
+
+function _filterWithPrice(json) {
+    return json.filter(piano => piano['price'] >= currentPriceMin && piano['price'] <= currentPriceMax)
+
+}
+
+function _filterWithBrand(json) {
+    return json.filter(piano => {
+        return piano.brand.toLowerCase() === currentBrand.toLowerCase();
+    });
+}
+
+function resetFilter(document) {
+    let resetButton = document.querySelector("#price-filter-reset-button");
+
+    resetButton.onclick = function () {
+        window.location.reload('/pianos.html');
+    }
+}
+
+function _sortDecider(json, sortBy, direction) {
+    let sorted;
+    if (!json) {
+        return;
+    }
+
+    if (direction === 'ascending') {
+        sorted = json.sort(function (a, b) {
+            if (a[sortBy] > b[sortBy]) {
+                return 1;
+            } else if (a[sortBy] < b[sortBy]) {
+                return -1;
+            } else {
+                return 0;
+            }
+        });
+    }
+
+    if (direction === 'descending') {
+        sorted = json.sort(function (a, b) {
+            if (a[sortBy] < b[sortBy]) {
+                return 1;
+            } else if (a[sortBy] > b[sortBy]) {
+                return -1;
+            } else {
+                return 0;
+            }
+        });
+    }
+    return sorted;
+}
---
+++ cn/static/pianoCn.js
@@ -0,0 +1,128 @@
+let constant = {
+    pianoRender: {
+        sortDir: "low_to_high",
+        priceMax: "200000",
+        priceMin: "0",
+        currentBrand: "all",
+
+    }
+};
+const pianosList = [
+
+	// 施坦威 STEINWAY & SONS
+    {
+        "uri": "../images/product/ste300183/1.jpg",
+        "price": 125000,
+        "date": "2021-8-13",
+        "modelName": "M170 安妮女王",
+        "brand": "施坦威 STEINWAY & SONS",
+        "link" : "ste300183.html"
+    },
+    {
+        "uri": "../images/product/ste93144/1.jpg",
+        "price": 128000,
+        "date": "2021-8-13",
+        "modelName": "A188 路易十六",
+        "brand": "施坦威 STEINWAY & SONS",
+        "link" : "ste93144.html"
+    },
+	{
+        "uri": "../images/product/ste261296/1.jpg",
+        "price": 78000,
+        "date": "2021-8-13",
+        "modelName": "M170",
+        "brand": "施坦威 STEINWAY & SONS",
+        "link" : "ste261296.html"
+    },
+	{
+        "uri": "../images/product/ste369476/1.jpg",
+        "price": 83000,
+        "date": "2021-8-13",
+        "modelName": "L180",
+        "brand": "施坦威 STEINWAY & SONS",
+        "link" : "ste369476.html"
+    },
+	{
+        "uri": "../images/product/ste193751/1.jpg",
+        "price": 78000,
+        "date": "2021-8-13",
+        "modelName": "M170",
+        "brand": "施坦威 STEINWAY & SONS",
+        "link" : "ste193751.html"
+    },
+	{
+        "uri": "../images/product/ste285121/1.jpg",
+        "price": 78000,
+        "date": "2021-8-13",
+        "modelName": "M170",
+        "brand": "施坦威 STEINWAY & SONS",
+        "link" : "ste285121.html"
+    },
+	
+	// 博兰斯勒 Blüthner
+	
+    {
+        "uri": "../images/product/blu111378/2.jpg",
+        "price": 88800,
+        "date": "2021-8-13",
+        "modelName": "Mod.10",
+        "brand": "博兰斯勒 Blüthner",
+        "link" : "blu111378.html"
+    },
+	
+	// Other Euro Brand
+	
+	{
+        "uri": "../images/product/gro93956/1.jpg",
+        "price": 140000,
+        "date": "2021-8-13",
+        "modelName": "Concerto G-275",
+        "brand": "Grotrian—Steinweg",
+        "link" : "gro93956.html"
+    },
+	
+	// YAMAHA
+	
+    {
+        "uri": "../images/product/yam5418344/1.jpg",
+        "price": 12000,
+        "date": "2021-8-13",
+        "modelName": "HQ300B",
+        "brand":"雅马哈 YAMAHA",
+        "link" : "yam5418344.html"
+    },
+	{
+        "uri": "../images/product/yam5209341/1.jpg",
+        "price": 10000,
+        "date": "2021-8-13",
+        "modelName": "HQ100",
+        "brand":"雅马哈 YAMAHA",
+        "link" : "yam5209341.html"
+    },
+    {
+        "uri": "../images/product/yam5249006/1.jpg",
+        "price": 12000,
+        "date": "2021-8-13",
+        "modelName": "HQ300",
+        "brand":"雅马哈 YAMAHA",
+        "link" : "yam5249006.html"
+    },
+	
+	// OHHASHI
+    {
+        "uri": "../images/product/ohh3054/1.jpg",
+        "price": 12000,
+        "date": "2021-8-13",
+        "modelName": "No.132",
+        "brand": "大桥 OHHASHI",
+        "link" : "ohh3054.html"
+    },
+	{
+        "uri": "../images/product/ohh2301/1.jpg",
+        "price": 12000,
+        "date": "2021-8-13",
+        "modelName": "No.132",
+        "brand": "大桥 OHHASHI",
+        "link" : "ohh2301.html"
+    }
+];
