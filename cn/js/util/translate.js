97977afb5217333e3a60337754af470ea8208031
lilu <laiapianosg@gmail.com>
2021-12-02 19:58:02 +0800

    translate for mobile

--- cn/js/util/translate.js
+++ cn/js/util/translate.js
@@ -3,4 +3,6 @@ window.onload = function(){
 		let fileName = arr[arr.length - 1];
 		let insertDiv = document.getElementById("translate");
 		  insertDiv.innerHTML = `<a href="../${fileName}">Switch to：English</a>`;
+		let insertDivMobile = document.getElementById("translateMobile");
+		  insertDivMobile.innerHTML = `<a href="../${fileName}" >Switch to：English</a>`;
 		}
--- js/util/translate.js
+++ js/util/translate.js
@@ -3,4 +3,6 @@ window.onload = function(){
 		let fileName = arr[arr.length - 1];
 		let insertDiv = document.getElementById("translate");
 		  insertDiv.innerHTML = `<a href="cn/${fileName}" >切换：中文(简体)</a>`;
+		let insertDivMobile = document.getElementById("translateMobile");
+		  insertDivMobile.innerHTML = `<a href="cn/${fileName}" >切换：中文(简体)</a>`;
 		}
