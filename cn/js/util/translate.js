4053c70623c1f76e465c5607e7ca8a887abdbf67
lilu <laiapianosg@gmail.com>
2025-04-22 15:45:14 +0800

    add routing for translate

--- cn/js/util/translate.js
+++ cn/js/util/translate.js
@@ -1,9 +1,24 @@
 // this method takes effective
 window.onload = function(){
+
+	
 		let arr = location.href.split('/');
 		let fileName = arr[arr.length - 1];
+
+		let path = "";
+
+		switch (fileName) {
+			case "contact.html":
+				path = "../../../contact.html"; // correct path
+				break;
+		
+			default:
+				path = `../../../pianos/${fileName}`;
+				break;
+		}
+
 		let insertDiv = document.getElementById("translate");
-		  insertDiv.innerHTML = `<a href="../../../pianos/${fileName}">Switch to: English</a>`;
+		insertDiv.innerHTML = `<a href="${path}">Switch to: English</a>`;
 		let insertDivMobile = document.getElementById("translateMobile");
-		  insertDivMobile.innerHTML = `<a href="../../../pianos/${fileName}" >Switch to: English</a>`;
+		insertDivMobile.innerHTML = `<a href="${path}" >Switch to: English</a>`;
 		}
--- js/util/translate.js
+++ js/util/translate.js
@@ -2,8 +2,30 @@
 window.onload = function(){
 		let arr = location.href.split('/');
 		let fileName = arr[arr.length - 1];
-		let insertDiv = document.getElementById("translate");
-		insertDiv.innerHTML = `<a href="../../cn/pianos/${fileName}" >切换：中文(简体)</a>`;
-		let insertDivMobile = document.getElementById("translateMobile");
-			insertDivMobile.innerHTML = `<a href="../../cn/pianos/${fileName}" >切换：中文(简体)</a>`;
+		let path = "";
+
+		switch (fileName) {
+			case "contact.html":
+				path = "../../cn/contact.html"; // correct path
+				break;
+		
+			default:
+				path = `../../cn/pianos/${fileName}`;
+				break;
 		}
+
+		let insertDiv = document.getElementById("translate");
+		insertDiv.innerHTML = `<a href="${path}" >切换：中文(简体)</a>`;
+		let insertDivMobile = document.getElementById("translateMobile");
+			insertDivMobile.innerHTML = `<a href="${path}" >切换：中文(简体)</a>`;
+		}
+
+/**
+ * filename = ""
+ * 
+ * switch  case 
+ * 	 contact -> path to contact.html
+ *   index
+ *   ..
+ * 	 defatul -> ../../cn/pianos/${fileName}
+ */
